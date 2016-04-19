76d95b586ec6641eb69469e4cbcf636fd4d85fe5
ansilva <anderson.nuneseth@gmail.com>
2016-04-19 16:49:35 -0300

    implementado o isLoaded para evitar carregar o baseComponent diversas vezes

--- src/module/Services/BaseComponentService.php
+++ src/module/Services/BaseComponentService.php
@@ -10,12 +10,22 @@ namespace Girolando\BaseComponent\Services;
 
 abstract class BaseComponentService
 {
+    protected static $baseJsLoaded = false;
+    protected static $loadedComponents = [];
     protected abstract function _init($params = []);
 
     public function init($params = [])
     {
+        $isLoaded = self::$baseJsLoaded;
+        self::$baseJsLoaded = true;
         $customView = $this->_init($params);
-        return view('_GHBaseComponent::Services.BaseComponentService.init', ['customView' => $customView]);
+        $registered = get_class($this);
+        $componentLoaded = false;
+        if(isset(self::$loadedComponents[$registered])){
+            return;
+        }
+        self::$loadedComponents[$registered] = true;
+        return view('_GHBaseComponent::Services.BaseComponentService.init', ['customView' => $customView, 'isLoaded' => $isLoaded, 'componentLoaded' => $componentLoaded]);
     }
 
 }
